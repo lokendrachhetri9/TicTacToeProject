bb26165f87facb77fe94ca74b327013341dab02c
SajanaB <92946666+SajanaB@users.noreply.github.com>
2021-10-27 14:55:22 -0400

    preliminary assignment

---
+++ Prelude-sajana.java
@@ -0,0 +1,27 @@
+//Programmer name: Sajana Balal
+
+public class Prelude {
+
+	public static void main(String[] args) {
+		// variable declaration
+		char c = '*';
+for(int j=0; j<3; j++) {	
+		
+	 for(int i=0; i<3; i++) {
+		 
+		System.out.print(c);
+		
+		if (i<2) {
+			System.out.print(" ");
+		}
+	 }
+	 
+ if(j<2) {
+ System.out.println();
+	}
+ 
+	}
+ 
+	}
+
+}
